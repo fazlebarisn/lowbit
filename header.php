34119cf00c817fb4213b1bdf6a4853e4d79eb7be
Fazle Bari <fazlebarisn@hotmail.com>
2023-01-07 09:17:12 +0600

    upload

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Header file for the Twenty Twenty WordPress default theme.
+ *
+ * @package lowbit
+ * @since 1.0
+ */
+?>
+
+<!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @package lowbit
+ * @since 1.0
+ */
+
+get_header();
+?>
+
+ ?>
+
+    
+</body>
+</html>
