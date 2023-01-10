613567e155b77f45368a06f6095e9df201692871
Fazle Bari <fazlebarisn@hotmail.com>
2023-01-10 23:21:09 +0600

    add_theme_support

--- functions.php
+++ functions.php
@@ -21,33 +21,65 @@ if( !function_exists( 'lowbit_theme_support' ) ){
 
     function lowbit_theme_support(){
 
-        // load theme textdomain
+        /*
+        * Make theme available for translation.
+        * Translations can be filed in the /languages/ directory.
+        * If you're building a theme based on Twenty Twenty, use a find and replace
+        * to change 'twentytwenty' to the name of your theme in all the template files.
+        */
         load_theme_textdomain( 'lowbit', get_template_directory(). '/languages' );
 
-        // add default comment andd RSS feed links to head
+        // Add default posts and comments RSS feed links to head.
         add_theme_support( 'automatic-feed-links' );
 
-        // let wordpress manage the document tag
+        /*
+        * Let WordPress manage the document title.
+        * By adding theme support, we declare that this theme does not use a
+        * hard-coded <title> tag in the document head, and expect WordPress to
+        * provide it for us.
+        */
         add_theme_support( 'title-tag' );
 
-        // enabel post thumbnail
+        // Enable support for Post Thumbnails on posts and pages.
         add_theme_support( 'post-thumbnails' );
 
-       // custom background
+       // Custom background color.
        add_theme_support( 'custom-background', apply_filters( 'lowbit_custom_bg_args', array(
            'default-color' => 'ffffff',
            'default-image' => ''
        ) ) ); 
 
-        // himl5 support
-        add_theme_support( 'html5', array(
-            'search-form',
-            'comment-form',
-            'comment-list',
-            'gallery',
-            'caption'
-        ) );
-       
+        /*
+        * Switch default core markup for search form, comment form, and comments
+        * to output valid HTML5.
+        */
+        add_theme_support(
+            'html5',
+            array(
+                'search-form',
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+                'script',
+                'style',
+                'navigation-widgets',
+            )
+        );
+
+        // Add theme support for selective refresh for widgets.
+        add_theme_support( 'customize-selective-refresh-widgets' );
+
+        // custom logo
+        add_theme_support(
+            'custom-logo',
+            array(
+                'height'      => 250,
+                'width'       => 250,
+                'flex-height' => true,
+                'flex-width'  => true,
+            )
+        );
     }
 }
 
