f9e79ec7fd35d198b5d40877b6ca1c745aa348ab
Fazle Bari <fazlebarisn@hotmail.com>
2023-01-14 22:56:58 +0600

    nav menu

--- functions.php
+++ functions.php
@@ -99,6 +99,7 @@ if( !function_exists( 'lowbit_theme_support' ) ){
         // Rrgister menu
         register_nav_menus( array(
             'primary' => esc_html__( 'Primary', 'lowbit' ),
+            'footer' => esc_html__( 'Footer', 'lowbit' ),
         ) );
     }
 }
